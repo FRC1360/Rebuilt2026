5b3c08a4119033875a64dc74a2bc054874b7354e
sidd <siddharthgowthaman@1360.ca>
2026-01-17 15:57:22 -0500

    Added getter and setter methods
    Added getter methods for all the motors and configs and also added a setter method for the kickup speed.

--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -9,6 +9,7 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,30 +17,76 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IndexSubsystem extends SubsystemBase {
   /** Creates a new IndexSubsystem. */
 
-  SparkFlex rightMotor;
-  SparkFlex leftMotor;
-  int rightMotorID = 0;
-  int leftMotorID = 0;
-  SparkMaxConfig leftConfig;
-  SparkMaxConfig rightConfig;
-  boolean inverted;
+  private SparkFlex rightMotor;
+  private SparkFlex leftMotor;
+  private SparkFlex kickupMotor;
+  private int rightMotorID = 0;
+  private int leftMotorID = 0;
+  private int kickupID = 0;
+  private SparkFlexConfig leftConfig;
+  private SparkFlexConfig rightConfig;
+  private SparkFlexConfig kickupConfig;
+  private boolean inverted;
+  private double kickupSpeed = 0.3;
 
   public IndexSubsystem() {
     rightMotor = new SparkFlex(rightMotorID, MotorType.kBrushless);
     leftMotor = new SparkFlex(leftMotorID, MotorType.kBrushless);
-    leftConfig = new SparkMaxConfig();
-    rightConfig = new SparkMaxConfig();
+    kickupMotor = new SparkFlex(kickupID, MotorType.kBrushless);
 
-    leftConfig.inverted(inverted);
-    rightConfig.idleMode(IdleMode.kBrake);
-    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftConfig = new SparkFlexConfig();
+    rightConfig = new SparkFlexConfig();
+    kickupConfig = new SparkFlexConfig();
 
     rightConfig.idleMode(IdleMode.kBrake);
     rightMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    leftConfig.inverted(inverted);
+    leftConfig.idleMode(IdleMode.kBrake);
+    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    kickupConfig.idleMode(IdleMode.kBrake);
+    kickupMotor.configure(kickupConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  public void setMotorSpeed(double speed) {
+  public void setIndexSpeed(double speed) {
     rightMotor.set(speed);
     leftMotor.set(speed);
+  }
+
+  public void turnKickup() {
+    kickupMotor.set(kickupSpeed);
+  }
+
+  public void setKickupSpeed(double speed) {
+    kickupSpeed = speed;
+  }
+
+  public SparkFlex getRightMotor() {
+    return rightMotor;
+  }
+
+  public SparkFlex getLeftMotor() {
+    return leftMotor;
+  }
+
+  public SparkFlex getKickupMotor() {
+    return kickupMotor;
+  }
+
+  public SparkFlexConfig getRightConfig() {
+    return rightConfig;
+  }
+
+  public SparkFlexConfig getLeftConfig() {
+    return leftConfig;
+  }
+
+  public SparkFlexConfig getKickupConfig() {
+    return kickupConfig;
+  }
+
+  public double getKickupSpeed() {
+    return kickupSpeed;
   }
 }
