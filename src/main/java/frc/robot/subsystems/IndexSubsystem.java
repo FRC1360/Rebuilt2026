a2991e90df8f938939a3de2ff249bd6dc32da9dc
sidd <siddharthgowthaman@1360.ca>
2026-01-17 15:49:10 -0500

    Edited Index Subsystem
    Made configs for all motors, set them to brake mode, and inverted the left motor in a different way. Added a kickup motor and a method that turns it on to a fixed speed.

--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -4,21 +4,38 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexSubsystem extends SubsystemBase {
   /** Creates a new IndexSubsystem. */
 
   SparkFlex rightMotor;
   SparkFlex leftMotor;
+  int rightMotorID = 0;
+  int leftMotorID = 0;
+  SparkMaxConfig leftConfig;
+  SparkMaxConfig rightConfig;
+  boolean inverted;
 
   public IndexSubsystem() {
-    rightMotor = new SparkFlex(0, MotorType.kBrushless);
-    leftMotor = new SparkFlex(0, MotorType.kBrushless);
-    
-    leftMotor.setInverted(true);
+    rightMotor = new SparkFlex(rightMotorID, MotorType.kBrushless);
+    leftMotor = new SparkFlex(leftMotorID, MotorType.kBrushless);
+    leftConfig = new SparkMaxConfig();
+    rightConfig = new SparkMaxConfig();
+
+    leftConfig.inverted(inverted);
+    rightConfig.idleMode(IdleMode.kBrake);
+    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    rightConfig.idleMode(IdleMode.kBrake);
+    rightMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void setMotorSpeed(double speed) {
