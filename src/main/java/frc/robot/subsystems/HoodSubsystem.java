f37dd988bf72f693f7629ba367ce8e61ca62d555
11311W <jw0062666@gmail.com>
2026-01-20 20:37:27 -0500

    Small edits
    Changed the need to input a motor id into the constructor and gave it a definite id value.
    
    Also added a gear ratio constant at the top.

--- src/main/java/frc/robot/subsystems/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/HoodSubsystem.java
@@ -5,8 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import com.revrobotics.spark.SparkMax;
-
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -18,22 +16,27 @@ import com.ctre.phoenix6.hardware.TalonFX;
  
 public class HoodSubsystem extends SubsystemBase {
   private TalonFX hoodmotor;
-  public TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
-  public Slot0Configs slot0Configs = talonFXConfigs.Slot0;
+  private int hoodMotorID = 0;
+  private int gearRatio = 0;
+  private TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+  private Slot0Configs slot0Configs = talonFXConfigs.Slot0;
 
   
   /** Creates a new HoodSubsystem. */
+
   
-  public HoodSubsystem(TalonFX hoodmotor) {
+  public HoodSubsystem() {
+    hoodmotor = new TalonFX(hoodMotorID);
     var hoodMConfigs = new TalonFXConfiguration();
-    this.hoodmotor = hoodmotor;
-    slot0Configs.kP = 2.4; // An error of 1 rotation results in 2.4 V output
-    slot0Configs.kI = 0; // no output for integrated error
-    slot0Configs.kD = 0.1;     // A velocity of 1 rps results in 0.1 V output
+
+    slot0Configs.kP = 0.0; // An error of 1 rotation results in 2.4 V output
+    slot0Configs.kI = 0.0; // no output for integrated error
+    slot0Configs.kD = 0.0;     // A velocity of 1 rps results in 0.1 V output
     slot0Configs.kS = 0.0;
     slot0Configs.kV = 0.0;
     slot0Configs.kG = 0.0;
     slot0Configs.kA = 0.0;
+
     MotionMagicConfigs hoodConfigs = hoodMConfigs.MotionMagic;
     hoodConfigs.MotionMagicCruiseVelocity = 0.0;
     hoodConfigs.MotionMagicExpo_kV = 0.0;
@@ -44,8 +47,8 @@ public class HoodSubsystem extends SubsystemBase {
 
   void MoveToAngle(double angle,double speed){
     
-    MotionMagicVoltage m_request = new MotionMagicVoltage(0);
-    double rotation = angle/360; // turn degrees into rotation for built in PID
+    MotionMagicVoltage m_request = new MotionMagicVoltage(0); 
+    double rotation = (angle/360)*gearRatio; // turn degrees into rotation for built in PID
     hoodmotor.setControl(m_request.withPosition(rotation));
   }
 
