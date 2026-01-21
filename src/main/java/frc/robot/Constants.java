96e1e657b9e342fdd576364be5b334de0e566df8
sidd <siddharthgowthaman@1360.ca>
2026-01-20 20:48:36 -0500

    Updated Subsystem and Constants
    Made an IndexConstants subclass and put the motor can ids into it. Changed the names of the motors and deleted unneeded getter methods. Also added motor speeds to smart dashboard and control each motor independently.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,10 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class IndexConstants {
+    public static final int hopperConveyorID = 0;
+    public static final int magazineConveyorID = 0;
+    public static final int magazineRollerID = 0;
+  }
 }
--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -10,83 +10,52 @@ import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class IndexSubsystem extends SubsystemBase {
   /** Creates a new IndexSubsystem. */
 
-  private SparkFlex rightMotor;
-  private SparkFlex leftMotor;
-  private SparkFlex kickupMotor;
-  private int rightMotorID = 0;
-  private int leftMotorID = 0;
-  private int kickupID = 0;
-  private SparkFlexConfig leftConfig;
-  private SparkFlexConfig rightConfig;
-  private SparkFlexConfig kickupConfig;
-  private boolean inverted;
-  private double kickupSpeed = 0.3;
+  private SparkFlex hopperConveyorMotor;
+  private SparkFlex magazineConveyorMotor;
+  private SparkFlex magazineRollerMotor;
+
+  private SparkFlexConfig hopperConveyorConfig;
+  private SparkFlexConfig magazineConveyorConfig;
+  private SparkFlexConfig magazineRollerConfig;
 
   public IndexSubsystem() {
-    rightMotor = new SparkFlex(rightMotorID, MotorType.kBrushless);
-    leftMotor = new SparkFlex(leftMotorID, MotorType.kBrushless);
-    kickupMotor = new SparkFlex(kickupID, MotorType.kBrushless);
+    hopperConveyorMotor = new SparkFlex(Constants.IndexConstants.hopperConveyorID, MotorType.kBrushless);
+    magazineConveyorMotor = new SparkFlex(Constants.IndexConstants.magazineConveyorID, MotorType.kBrushless);
+    magazineRollerMotor = new SparkFlex(Constants.IndexConstants.magazineRollerID, MotorType.kBrushless);
 
-    leftConfig = new SparkFlexConfig();
-    rightConfig = new SparkFlexConfig();
-    kickupConfig = new SparkFlexConfig();
+    hopperConveyorConfig = new SparkFlexConfig();
+    magazineConveyorConfig = new SparkFlexConfig();
+    magazineRollerConfig = new SparkFlexConfig();
 
-    rightConfig.idleMode(IdleMode.kBrake);
-    rightMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    magazineConveyorConfig.idleMode(IdleMode.kBrake);
+    hopperConveyorMotor.configure(magazineConveyorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    leftConfig.inverted(inverted);
-    leftConfig.idleMode(IdleMode.kBrake);
-    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    hopperConveyorConfig.idleMode(IdleMode.kBrake);
+    magazineConveyorMotor.configure(hopperConveyorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    kickupConfig.idleMode(IdleMode.kBrake);
-    kickupMotor.configure(kickupConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    magazineRollerConfig.idleMode(IdleMode.kBrake);
+    magazineRollerMotor.configure(magazineRollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  public void setIndexSpeed(double speed) {
-    rightMotor.set(speed);
-    leftMotor.set(speed);
+  public void setHopperConveyorSpeed(double speed) {
+    hopperConveyorMotor.set(speed);
+    SmartDashboard.putNumber("Subsystems/IndexSubsystem/hopperConveyorSpeed", speed);
   }
 
-  public void turnKickup() {
-    kickupMotor.set(kickupSpeed);
+  public void setMagazineConveyorSpeed(double speed) {
+    magazineConveyorMotor.set(speed);
+    SmartDashboard.putNumber("Subsystems/IndexSubsystem/magazineConveyorSpeed", speed);
   }
 
-  public void setKickupSpeed(double speed) {
-    kickupSpeed = speed;
-  }
-
-  public SparkFlex getRightMotor() {
-    return rightMotor;
-  }
-
-  public SparkFlex getLeftMotor() {
-    return leftMotor;
-  }
-
-  public SparkFlex getKickupMotor() {
-    return kickupMotor;
-  }
-
-  public SparkFlexConfig getRightConfig() {
-    return rightConfig;
-  }
-
-  public SparkFlexConfig getLeftConfig() {
-    return leftConfig;
-  }
-
-  public SparkFlexConfig getKickupConfig() {
-    return kickupConfig;
-  }
-
-  public double getKickupSpeed() {
-    return kickupSpeed;
+  public void setMagazineRollerSpeed(double speed) {
+    magazineRollerMotor.set(speed);
+    SmartDashboard.putNumber("Subsystems/IndexSubsystem/magazineRollerSpeed", speed);
   }
 }
