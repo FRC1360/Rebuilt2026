3ccf5efd14b3262c65ec6f8e54c7ced918ac9c05
yashvedant-boop <yashvedant@1360.ca>
2026-02-17 19:18:30 -0500

    Update RetractIntakeCommand.java

--- src/main/java/frc/robot/commands/intake/RetractIntakeCommand.java
+++ src/main/java/frc/robot/commands/intake/RetractIntakeCommand.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.intake;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.Constants;
@@ -7,9 +9,12 @@ import frc.robot.Constants;
 public class RetractIntakeCommand extends Command {
 
     private final IntakeSubsystem intakeSubsystem;
+    private final Supplier<Boolean> runWheels;
 
-    public RetractIntakeCommand(IntakeSubsystem intakeSubsystem) {
+
+    public RetractIntakeCommand(IntakeSubsystem intakeSubsystem, Supplier<Boolean> runWheels) {
         this.intakeSubsystem = intakeSubsystem;
+        this.runWheels = runWheels;
         addRequirements(intakeSubsystem);
     }
 
@@ -24,7 +29,12 @@ public class RetractIntakeCommand extends Command {
         double output = intakeSubsystem.closedLoopCalculate(Constants.IntakeConstants.RETRACT_ANGLE, intakeSubsystem.getPidController().getSetpoint().velocity);
         intakeSubsystem.setPivotVoltage(output);
 
-        intakeSubsystem.setIntakeWheelSpeed(Constants.IntakeConstants.WHEEL_SPEED);
+        if (runWheels.get()) {
+    intakeSubsystem.setIntakeWheelSpeed(Constants.IntakeConstants.RETRACT_ANGLE);
+        } else {
+    intakeSubsystem.setIntakeWheelSpeed(0);
+        }
+
     }
 
     @Override
