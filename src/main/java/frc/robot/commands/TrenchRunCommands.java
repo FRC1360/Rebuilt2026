430192c1342262be1d1a71c4f66126e5a77ba25a
Bot106 <78245957+Bot-106@users.noreply.github.com>
2026-04-01 21:37:44 -0400

    Failed attempt to make it work on red alliance

--- src/main/java/frc/robot/commands/TrenchRunCommands.java
+++ src/main/java/frc/robot/commands/TrenchRunCommands.java
@@ -1,6 +1,9 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Rotation;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +13,8 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.RotationTarget;
+import com.pathplanner.lib.util.FlippingUtil;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -30,6 +35,8 @@ public class TrenchRunCommands {
     private static final StructArrayPublisher<Pose2d> trajectoryPublisher = loggingTable
             .getStructArrayTopic("Goal Pose Iterations", Pose2d.struct).publish();
 
+    private static final double INTRO_WAYPOINT_VELOCITY_ACCOUNTING_THRESHOLD = 0.01;
+
     public static Command goToAllianceFromNeutralHumanCommand(CommandSwerveDrivetrain drivetrain) {
 
         RobotState robotState = RobotState.getInstance();
@@ -40,20 +47,23 @@ public class TrenchRunCommands {
             startingPose = new Pose2d();
             middlePose = new Pose2d();
             endingPose = new Pose2d();
+            Rotation2d goalEndRotation = new Rotation2d();
+            double goalEndVelocityMps = 2.0;
 
             Rotation2d robotRotation = currentRobotPose.getRotation();
             double robotRotationDegrees = MathUtil.inputModulus(robotRotation.getDegrees(), -180, 180);
 
-            if (robotState.getFieldRelativeRobotVelocityVector().getNorm() < 0.01) {
-                startingPose = new Pose2d(
-                        currentRobotPose.getTranslation(),
-                        PhotonUtils.getYawToPose(
-                                new Pose2d(currentRobotPose.getTranslation(), new Rotation2d()),
-                                new Pose2d(FieldConstants.BLUE_HUMAN_TRENCH_NEUTRAL_ENTRY, new Rotation2d())));
-            } else {
-                startingPose = new Pose2d(
-                        currentRobotPose.getTranslation(),
-                        robotState.getFieldRelativeRobotVelocityVector().getAngle());
+            startingPose = new Pose2d(
+                    currentRobotPose.getTranslation(),
+                    PhotonUtils.getYawToPose(
+                            new Pose2d(currentRobotPose.getTranslation(), new Rotation2d()),
+                            new Pose2d(FieldConstants.BLUE_HUMAN_TRENCH_NEUTRAL_ENTRY, new Rotation2d())));
+
+            if (!robotState.isBlueAlliance.getAsBoolean()) {
+                currentRobotPose = FlippingUtil.flipFieldPose(currentRobotPose);
+                startingPose = FlippingUtil.flipFieldPose(startingPose);
+                robotRotation = currentRobotPose.getRotation();
+                robotRotationDegrees = MathUtil.inputModulus(robotRotation.getDegrees(), -180, 180);
             }
 
             if (robotRotationDegrees <= -45 && robotRotationDegrees >= -135) {
@@ -65,6 +75,7 @@ public class TrenchRunCommands {
                         FieldConstants.BLUE_HUMAN_TRENCH_ALLIANCE_ENTRY.plus(
                                 FieldConstants.TRENCH_INTAKE_OFFSET),
                         Rotation2d.k180deg);
+                goalEndRotation = Rotation2d.kCW_90deg;
             } else if (robotRotationDegrees >= 45 && robotRotationDegrees <= 135) {
                 middlePose = new Pose2d(
                         FieldConstants.BLUE_HUMAN_TRENCH_NEUTRAL_ENTRY.minus(
@@ -74,6 +85,7 @@ public class TrenchRunCommands {
                         FieldConstants.BLUE_HUMAN_TRENCH_ALLIANCE_ENTRY.minus(
                                 FieldConstants.TRENCH_INTAKE_OFFSET),
                         Rotation2d.k180deg);
+                goalEndRotation = Rotation2d.kCCW_90deg;
             } else {
                 middlePose = new Pose2d(
                         FieldConstants.BLUE_HUMAN_TRENCH_NEUTRAL_ENTRY,
@@ -81,13 +93,31 @@ public class TrenchRunCommands {
                 endingPose = new Pose2d(
                         FieldConstants.BLUE_HUMAN_TRENCH_ALLIANCE_ENTRY,
                         Rotation2d.k180deg);
+                if ((robotRotationDegrees >= 90 && robotRotationDegrees <= 180) ||
+                        (robotRotationDegrees <= -90 && robotRotationDegrees >= -180))
+                    goalEndRotation = Rotation2d.k180deg;
+                else
+                    goalEndRotation = Rotation2d.kZero;
             }
+
+            double distanceToTrench, distanceThroughTrench, totalDistanceTravelled;
+            distanceToTrench = startingPose.getTranslation().getDistance(middlePose.getTranslation());
+            distanceThroughTrench = middlePose.getTranslation().getDistance(endingPose.getTranslation());
+            totalDistanceTravelled = distanceToTrench + distanceThroughTrench;
+            RotationTarget rotationTargetAtTrenchEntry = new RotationTarget(
+                    distanceToTrench / totalDistanceTravelled,
+                    goalEndRotation);
 
             PathPlannerPath path = new PathPlannerPath(
                     PathPlannerPath.waypointsFromPoses(startingPose, middlePose, endingPose),
+                    List.of(rotationTargetAtTrenchEntry),
+                    Collections.emptyList(),
+                    Collections.emptyList(),
+                    Collections.emptyList(),
                     trenchRunConstraints,
                     null,
-                    new GoalEndState(0, Rotation2d.k180deg));
+                    new GoalEndState(goalEndVelocityMps, goalEndRotation),
+                    false);
 
             trajectoryPublisher.accept(path.getPathPoses().toArray(Pose2d[]::new));
 
